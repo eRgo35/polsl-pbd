a050cb512fd58c8d2f46f79f157d6c9a0f95f026
Michael Czyż <mike@c2yz.com>
2024-05-01 21:24:12 +0200

    lab2 finish

--- SQL2/lab2.sql
+++ SQL2/lab2.sql
@@ -10,24 +10,11 @@
 
 -- 1. Dla każdego pracownika odpowiedzialnego za jakiś przedmiot podaj liczbę przedmiotów
 -- z których prowadzi zajęcia (tabela ROZKLADY).
-SELECT pr.nr_odp_prac, p.nazwisko, COUNT(r.nr_przedm) AS il_przedm
-FROM rozklady r
-INNER JOIN pracownicy p
-ON r.nr_prac = p.nr_prac
-INNER JOIN przedmioty pr
-ON p.nr_prac = pr.nr_odp_prac
-WHERE r.nr_przedm = pr.nr_przedm
-GROUP BY pr.nr_odp_prac, p.nazwisko
-HAVING COUNT(r.nr_przedm) > 0
---! WIP counting doesn't work as intended (idk why)
-SELECT pr.NR_PRAC, pr.nazwisko, COUNT(r.nr_przedm) AS ile_przedm
-FROM ROZKLADY r
-INNER JOIN PRZEDMIOTY p ON p.NR_PRZEDM = r.NR_PRZEDM
-INNER JOIN PRACOWNICY pr ON pr.NR_PRAC = r.NR_PRAC
-WHERE r.NR_PRZEDM = p.NR_PRZEDM
-GROUP BY NR_PRAC, NAZWISKO
-HAVING COUNT(r.NR_PRZEDM) > 0
--- Hard bastard, another version but still broken
+SELECT pr.NR_PRAC, NAZWISKO, COUNT(DISTINCT r.NR_PRZEDM)
+FROM pracownicy pr
+INNER JOIN przedmioty p ON p.NR_ODP_PRAC = pr.NR_PRAC
+INNER JOIN rozklady r ON r.NR_PRAC = pr.NR_PRAC
+GROUP BY pr.NR_PRAC, NAZWISKO;
 
 -- 2. Podaj nazwiska i średnią ocen z przedmiotu ALGEBRA tych studentów, którzy z tego
 -- przedmiotu mają więcej niż jedną ocenę.
@@ -42,86 +29,64 @@ HAVING COUNT(OCENA) > 1;
 
 -- 3. Podaj przedmioty realizowane przez największą liczbę pracowników.
 
---! SOMETHING TO CHECK
-SELECT nr_przedm, nazwa_przedm 
-FROM przedmioty 
-GROUP BY nr_przedm, nazwa_przedm 
-HAVING COUNT(nr_odp_prac) = (
-    SELECT MAX(pracownik_count)
-    FROM (
-        SELECT COUNT(nr_odp_prac) AS pracownik_count
-        FROM przedmioty
-        GROUP BY nr_przedm
-    ) AS max_pracownik_count
-);
-
---HERE BE DRAGONS!!! 
--- not working for nr_przedm cuz somehow there's 2 of bazy danych
-SELECT z.NAZWA_PRZEDM, COUNT(p.NR_PRAC)
-FROM rozklady r
-INNER JOIN pracownicy p ON p.NR_PRAC = r.NR_PRAC
-INNER JOIN przedmioty z ON z.NR_PRZEDM = r.NR_PRZEDM
-GROUP BY z.NAZWA_PRZEDM
-HAVING COUNT(p.NR_PRAC) = (
-	SELECT MAX(MAXIMUM)
-	FROM (
-		SELECT COUNT(p.NR_PRAC) AS MAXIMUM
-		FROM rozklady r
-      INNER JOIN pracownicy p ON p.NR_PRAC = r.NR_PRAC
-      INNER JOIN przedmioty z ON z.NR_PRZEDM = r.NR_PRZEDM
-      GROUP BY z.NAZWA_PRZEDM
-	)
-	AS podzapytanie
-);
+SELECT p.NR_PRZEDM, p.NAZWA_PRZEDM
+FROM przedmioty p
+INNER JOIN rozklady r ON p.NR_PRZEDM = r.NR_PRZEDM
+INNER JOIN pracownicy pr ON pr.NR_PRAC = r.NR_PRAC
+GROUP BY p.NAZWA_PRZEDM, p.NR_PRZEDM
+ORDER BY COUNT(pr.NR_PRAC) DESC
+LIMIT 1;
 
 -- 4. Podaj numery i nazwiska pracowników, którzy w temacie DYSK otrzymali wypłatę mniejszą
 -- od co najmniej jednej z wypłat pracownika o nazwisku GRZYBEK w dowolnym temacie.u
 
-SELECT p.nr_prac, p.NAZWISKO
-FROM pracownicy p
-INNER JOIN wyplaty w ON p.NR_PRAC = w.NR_PRAC
-INNER JOIN tematy t ON w.NR_TEM = t.NR_TEM
-WHERE TEMAT LIKE 'DYSK'
-GROUP BY p.nr_prac, p.NAZWiSKO
-HAVING w.KWOTA < (
-	SELECT KWOTA
-	FROM pracownicy p
-	INNER JOIN wyplaty w ON w.NR_PRAC = p.NR_PRAC
-	INNER JOIN TEMATY t ON t.NR_TEM = w.NR_TEM
-	WHERE NAZWISKO LIKE 'GRZYBEK'
-);
+SELECT DISTINCT p1.nr_prac, p1.NAZWISKO
+FROM pracownicy p1
+INNER JOIN wyplaty w1 ON p1.NR_PRAC = w1.NR_PRAC
+INNER JOIN tematy t ON w1.NR_TEM = t.NR_TEM
+INNER JOIN pracownicy p2 ON p2.NAZWISKO LIKE 'GRZYBEK'
+INNER JOIN wyplaty w2 ON p2.NR_PRAC = w2.NR_PRAC
+WHERE TEMAT LIKE 'DYSK';
 
 -- 5. Podaj numery, nazwiska i daty urodzenia mężczyzn zatrudnionych w zespole OPROGRAMOWANIE,
 -- którzy są młodsi od każdego pracownika zespołu BUDOWA
 
 SELECT nr_prac, nazwisko, data_ur
 FROM pracownicy p
-INNER JOIN zespoly z ON p.nr_zesp = z.nr_zesp
-WHERE plec LIKE 'M' AND z.nazwa_zesp LIKE 'OPROGRAMOWANIE'
-GROUP BY nr_prac, nazwisko, data_ur
-HAVING data_ur > (
-    SELECT MIN(p1.data_ur)
-    FROM pracownicy p1
-    INNER JOIN zespoly z1 ON p1.nr_zesp = z1.nr_zesp
-    WHERE z1.nazwa_zesp LIKE 'BUDOWA'
-)
-ORDER BY data_ur DESC
-LIMIT 1;
+INNER JOIN zespoly z ON z.NR_ZESP = p.NR_ZESP
+WHERE PLEC LIKE 'M'
+AND nazwa_zesp LIKE 'OPROGRAMOWANIE'
+AND data_ur > (
+	SELECT MAX(data_ur)
+	FROM pracownicy p
+	INNER JOIN zespoly z ON p.nr_zesp = z.nr_zesp
+	WHERE nazwa_zesp LIKE 'BUDOWA'
+);
+------------------------------------------------------------------------
+-- 6. Podaj nazwiska najstarszych studentów na poszczególnych kierunkach.
 
--- 6.
-
-
+SELECT k.nr_kier, nazwa_kier, nr_stud, nazwisko
+FROM kierunki k
+INNER JOIN studenci s ON k.nr_kier = s.nr_kier
+INNER JOIN (
+    SELECT nr_kier, MIN(data_ur) AS min_data_ur
+    FROM studenci
+    GROUP BY nr_kier
+) d ON s.nr_kier = d.nr_kier AND s.data_ur = d.min_data_ur;
 
 -- 7. Podaj nazwy zespołów, w których nie pracuje żadna kobieta. 
-SELECT nazwa_zesp
+SELECT z.NAZWA_ZESP
 FROM zespoly z
-LEFT JOIN pracownicy p
-ON z.nr_zesp = p.nr_zesp
-GROUP BY nazwa_zesp
-HAVING SUM(CASE WHEN p.plec = 'K' THEN 1 ELSE 0 END) = 0;
+LEFT JOIN pracownicy p 
+ON z.NR_ZESP = p.NR_ZESP AND p.PLEC = 'K'
+WHERE p.NR_ZESP IS NULL;
 
 -- 8.
-
+SELECT z.nr_zesp, nazwa_zesp, COUNT(nr_prac) AS ile
+FROM zespoly z
+LEFT JOIN pracownicy p ON z.nr_zesp = p.nr_zesp
+GROUP BY z.nr_zesp, nazwa_zesp
+ORDER BY z.nr_zesp;
 
 -- 9. Utworzyć perspektywę zawierającą wyszczególnione informacje, oraz określić, czy można
 -- aktualizować dane w tej perspektywie. Jeżeli nie można - podać wszystkie tego przyczyny.
@@ -141,25 +106,70 @@ SELECT * FROM perspektywa;
 
 DROP VIEW perspektywa;
 
--- todo figure out if can update the view
+-- Nie jest możliwa modyfikacja perspektywy ponieważ w zapytaniu tworzącym perspektywę
+-- występuje złączenie wielu tabel oraz występuje słowo kluczone DISTINCT (czyli agregat kwerendy)
 
+-- 10.
+
+CREATE VIEW perspektywa2 AS
+SELECT z.NR_ZESP AS nrz, z.NAZWA_ZESP, p.NR_PRAC, p.NAZWISKO, SUM(w.KWOTA) AS suma_zarobkow_prac
+FROM zespoly z
+INNER JOIN pracownicy p ON z.NR_ZESP = p.NR_ZESP
+LEFT JOIN wyplaty w ON w.NR_PRAC = p.NR_PRAC
+GROUP BY z.NR_ZESP, z.NAZWA_ZESP, p.NR_PRAC, p.NAZWISKO;
+
+SELECT * FROM perspektywa2;
+
+SELECT nazwa_zesp, 
+AVG(suma_zarobkow_prac) AS 'sredni',
+MIN(suma_zarobkow_prac) AS 'min',
+MAX(suma_zarobkow_prac) AS 'max'
+FROM perspektywa2
+GROUP BY nazwa_zesp
+
+DROP VIEW perspektywa2;
+
+-- 11.
+
+SELECT DISTINCT nazwisko
+FROM pracownicy p
+INNER JOIN przydzialy r ON r.nr_prac = p.NR_PRAC
+LEFT JOIN wyplaty w ON p.NR_PRAC = w.NR_PRAC AND r.NR_TEM = w.NR_TEM
+WHERE kwota IS NULL
+
+-- 12.
+
+SELECT nr_prac, NAZWISKO, nr_tem, temat
+FROM tematy t
+INNER JOIN pracownicy p ON p.nr_prac = t.nr_prac_kt
+WHERE NOT EXISTS (
+	SELECT *
+	FROM przydzialy r
+	WHERE t.nr_prac_kt = r.nr_prac AND t.nr_tem = r.nr_tem AND
+	r.kod_funkcji LIKE 'KRW'
+);
+
+SELECT p.nr_prac, nazwisko, t.nr_tem, temat
+FROM pracownicy p
+INNER JOIN przydzialy r ON p.nr_prac = r.nr_prac
+INNER JOIN tematy t ON r.NR_TEM = t.NR_TEM
+WHERE kod_funkcji LIKE 'KRW' AND p.nr_prac NOT LIKE NR_PRAC_KT;
 
 -- 13.
 
--- not worky bc should use przydziały somehow
-SELECT nr_prac, nazwisko
-FROM pracownicy
-WHERE nr_prac NOT IN (
-    SELECT p.nr_prac
-    FROM pracownicy p
-    WHERE p.nazwisko LIKE 'NIEZALEZNY' NOT IN(
-        SELECT p1.nazwisko
-        FROM pracownicy p1
-        INNER JOIN tematy t
-        ON p1.nr_prac = t.nr_prac_kt
-    )
-)
-
+SELECT p.NR_PRAC, p.NAZWISKO
+FROM pracownicy p
+LEFT JOIN (
+    SELECT DISTINCT r.NR_PRAC
+    FROM przydzialy r
+    INNER JOIN (
+        SELECT DISTINCT r.NR_TEM
+        FROM przydzialy r
+        INNER JOIN pracownicy p ON p.NR_PRAC = r.NR_PRAC
+        WHERE p.NAZWISKO LIKE 'NIEZALEZNY'
+    ) sub ON r.NR_TEM = sub.NR_TEM
+) sub2 ON p.NR_PRAC = sub2.NR_PRAC
+WHERE sub2.NR_PRAC IS NULL;
 
 --14. Podaj nazwiska pracowników którzy realizowali wszystkie tematy, których
 -- kierownikiem jest JASKOLA.
